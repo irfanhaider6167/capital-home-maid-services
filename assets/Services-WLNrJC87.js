c7ec42076861794d5710f4f7dd3268bb60ec5c01
irfanhaider6167 <101730155+irfanhaider6167@users.noreply.github.com>
2026-05-11 16:00:32 +0500

    Updates

---
+++ assets/Services-WLNrJC87.js
@@ -0,0 +1,16 @@
+import{r as e,j as t,T as r,B as s,y as a}from"./index-tMKTCunX.js";import o from"./Card-BgZQoRwv.js";import"./CardContent-mDT9tIcD.js";const n=()=>t.jsxs(c,{children:[t.jsx(r,{variant:"h4",className:"title",children:"Our Services"}),t.jsx(s,{className:"content",children:t.jsx(o,{})})]}),c=a.section`
+  padding: 60px 20px;
+  background: #f9fafb;
+
+  .title {
+    text-align: center;
+    margin-bottom: 30px;
+    font-weight: 700;
+    color: #111827;
+  }
+
+  .content {
+    display: flex;
+    justify-content: center;
+  }
+`,l=e.memo(n);export{l as default};
